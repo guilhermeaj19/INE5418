d92386f4fe22adcaa878199d0c8229ab24e88715
guilhermeaj19 <guilhermeadenilson16@gmail.com>
2024-05-08 21:29:22 -0300

    adicionando tela de teste

--- INE5418-T1/user.py
+++ INE5418-T1/user.py
@@ -1,6 +1,11 @@
 # from multiprocessing import Process
 from threading import Thread
 import zmq
+from tkinter import *
+
+
+
+
 
 class User:
     def __init__(self, username):
@@ -17,27 +22,47 @@ class User:
 
         self.__receiving_thread = Thread(target=self.waiting_message)
 
+
+        self.__window = Tk()
+
+        self.__messages = Text(self.__window)
+        self.__messages.pack()
+
+        self.__input_user = StringVar()
+        self.__input_field = Entry(self.__window, text=self.__input_user)
+        self.__input_field.pack(side=BOTTOM, fill=X)
+
+        frame = Frame(self.__window)
+        self.__input_field.bind("<Return>", self.enter_pressed)
+        frame.pack()
+
+    def enter_pressed(self,event):
+        input_get = self.__input_field.get()
+    
+        self.writing_message(input_get)
+        self.__input_user.set('')
+
+        return "break"
+
     def start(self):
         self.__receiving_thread.start()
-        self.writing_message()
-        self.__receiving_thread.join()
-        print("Conversa finalizada")
+        self.__window.mainloop()
+        exit()
 
-    def writing_message(self):
-        while True:
-            message_to_send = input("Digite uma mensagem: ")
+    def writing_message(self, message_to_send):
 
-            data = {"user": self.__username, "message": message_to_send}
+        data = {"user": self.__username, "message": message_to_send}
 
-            self.__sending_mq.send_json(data)
-            print(self.__sending_mq.recv_string())
+        self.__sending_mq.send_json(data)
+        print(self.__sending_mq.recv_string())
 
     def waiting_message(self):
         while True:
             message = self.__receiving_mq.recv_json()
 
             name_user, text_received = message["user"], message["message"]
-            print(f"{name_user}: {text_received}")
+            self.__messages.insert(INSERT, '%s\n' % f"{name_user}: {text_received}")
+
 
 
 username = input("Insira seu nome: ")
