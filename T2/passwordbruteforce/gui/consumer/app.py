247c4b761cb14605b721f4ccdf3f181ac594ad56
Vicente Cardoso dos Santos <58831732+vicente-cs@users.noreply.github.com>
2024-07-01 14:52:05 -0300

    add archiver ip consumer

--- T2/passwordbruteforce/gui/consumer/app.py
+++ T2/passwordbruteforce/gui/consumer/app.py
@@ -4,16 +4,17 @@ from tkinter import ttk
 from passwordbruteforce.password_solver import PasswordSolver
 import threading
 
-class ConsumerApp(tk.Tk):
-    def selecionar_arquivo(self):
-        filedialogue = fd.askopenfilename(title="Selecione o arquivo")
-        return filedialogue
 
+class ConsumerApp(tk.Tk):
     def _init_passwdsolver(self):
-        self.passwd_solver = PasswordSolver(self.endereco_entry.get().split(","))
+        self.passwd_solver = PasswordSolver(
+            self.endereco_entry.get().split(","), self.archiver_entry.get()
+        )
         self.solve_label = tk.Label(text="Aguardando senha...")
         self.solve_label.grid(row=2, columnspan=2, sticky="nsew")
-        self.wait_solve = threading.Thread(target=self.passwd_solver.wait_password, args=(self._change_solve_label,))
+        self.wait_solve = threading.Thread(
+            target=self.passwd_solver.wait_password, args=(self._change_solve_label,)
+        )
         self.wait_solve.start()
 
     def _change_solve_label(self, _text):
@@ -22,11 +23,22 @@ class ConsumerApp(tk.Tk):
     def __init__(self, master=None):
         super().__init__(master)
         self.title("Consumer")
+
+        # Tuplespaces
         self.endereco_label = tk.Label(master, text="Tuplespaces IP:")
         self.endereco_entry = tk.Entry(master)
         self.endereco_label.grid(row=0, column=0, sticky=W, pady=1)
         self.endereco_entry.grid(row=0, column=1, sticky=W, pady=1)
 
+        # Archiver
+        self.archiver_label = tk.Label(master, text="Archiver IP:")
+        self.archiver_entry = tk.Entry(master)
+        self.archiver_label.grid(row=1, column=0, sticky=W, pady=1)
+        self.archiver_entry.grid(row=1, column=1, sticky=W, pady=1)
+        self.archiver_entry.insert(0, "tcp://127.0.0.1:63000")
+
         # Enviar
-        self.send_btn = tk.Button(master, text="Enviar", command=self._init_passwdsolver)
-        self.send_btn.grid(row=1, columnspan=2, sticky="nsew", pady=1)
+        self.send_btn = tk.Button(
+            master, text="Enviar", command=self._init_passwdsolver
+        )
+        self.send_btn.grid(row=2, columnspan=2, sticky="nsew", pady=1)
